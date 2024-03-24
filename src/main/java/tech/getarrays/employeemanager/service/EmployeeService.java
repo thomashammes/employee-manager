1fddf35bfbd66525c519f549e0baff86ecce6ed1
Thomas Hammes <thomas.hammes@verdecard.com.br>
2024-03-24 20:00:42 -0300

    Transactional notation added to deleteEmployee

--- src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
+++ src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -2,6 +2,7 @@ package tech.getarrays.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tech.getarrays.employeemanager.exception.EmployeeNotFoundException;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.repository.EmployeeRepository;
@@ -36,7 +37,9 @@ public class EmployeeService {
                 .orElseThrow(() -> new EmployeeNotFoundException("Employee by id " + id + " was not found"));
     }
 
+    @Transactional
     public void deleteEmployee(Long id) {
+        System.out.println("deleteEmployee in Employee Service. Id: " + id);
         employeeRepository.deleteEmployeeById(id);
     }
 }
